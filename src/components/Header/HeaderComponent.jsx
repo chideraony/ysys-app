b6f4543666986d42e724bf92b64a44830c5578c4
sindersella <sundus.abdi99@hotmail.com>
2021-03-07 20:14:46 +0000

    this is the header section so far, the search bar and submit button still need to be linked with the api

--- src/components/Header/HeaderComponent.jsx
+++ src/components/Header/HeaderComponent.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import './HeaderComponent.css';
+import { Button } from '@brandwatch/axiom-components';
+
 
 // TODO - make sure HeaderComponent is expecting the right props (if any)!
 export const HeaderComponent = () => {
@@ -7,6 +9,18 @@ export const HeaderComponent = () => {
         {/* TODO [STRETCH] - add in any controls that you'd like in your header
              E.g, a search bar, a toggle button for the side bar, or just a plain header!
         */}
-        I'm the header component
+     <ul className="pagelinks">
+			<li><a href="">Home</a></li>
+			<li><a href="">Characters</a></li>
+			<li><a href="">Houses</a></li>
+			<li><input className="searchBar"/></li>
+			<Button className="searchButton" variant="primary">
+	        	Search
+	        </Button>
+
+	</ul>
+	
+
+	
     </header>
 };
