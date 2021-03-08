0786404248995c595435ad6c353b5501bee37afd
HayFe72 <52582897+HayFe72@users.noreply.github.com>
2021-03-08 13:58:28 +0000

    Updated header section

--- src/components/Header/HeaderComponent.jsx
+++ src/components/Header/HeaderComponent.jsx
@@ -1,26 +1,19 @@
-import React from 'react';
-import './HeaderComponent.css';
-import { Button } from '@brandwatch/axiom-components';
-
+import React from "react";
+import "./HeaderComponent.css";
+import { Button } from "@brandwatch/axiom-components";
 
 // TODO - make sure HeaderComponent is expecting the right props (if any)!
 export const HeaderComponent = () => {
-    return <header className={'header'}>
-        {/* TODO [STRETCH] - add in any controls that you'd like in your header
+  return (
+    <header className={"header"}>
+      {/* TODO [STRETCH] - add in any controls that you'd like in your header
              E.g, a search bar, a toggle button for the side bar, or just a plain header!
         */}
-     <ul className="pagelinks">
-			<li><a href="">Home</a></li>
-			<li><a href="">Characters</a></li>
-			<li><a href="">Houses</a></li>
-			<li><input className="searchBar"/></li>
-			<Button className="searchButton" variant="primary">
-	        	Search
-	        </Button>
-
-	</ul>
-	
-
-	
+      <h1>A Better App of Ice and Fire</h1>
+      <input className="searchBar" />
+      <Button className="searchButton" variant="primary">
+        Search
+      </Button>
     </header>
+  );
 };
