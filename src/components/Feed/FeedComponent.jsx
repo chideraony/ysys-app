ec95a39eb462f113ff55d21e9fe94a3288f89fc2
HayFe72 <52582897+HayFe72@users.noreply.github.com>
2021-03-16 18:45:36 +0000

    Added message when feed is empty. Made minor styling changes and changed title. Tidied up readability in some files.

--- src/components/Feed/FeedComponent.jsx
+++ src/components/Feed/FeedComponent.jsx
@@ -3,40 +3,38 @@ import { BookCard } from "../STRETCH_Cards/BookCard";
 import { CharacterCard } from "../STRETCH_Cards/CharacterCard";
 import { HouseCard } from "../STRETCH_Cards/HouseCard";
 import "./FeedComponent.css";
-import {
-  Grid,
-  GridCell,
-} from "@brandwatch/axiom-components";
+import { Text } from "@brandwatch/axiom-components";
 
 // TODO - make sure FeedComponent is expecting the right props!
-export const FeedComponent = ({responseData, sidebarCategories}) => {
-  console.log(responseData)
+export const FeedComponent = ({ responseData, sidebarCategories }) => {
+  console.log(responseData);
   return (
     <div className={"feed"}>
-
-
       {/* TODO - add a placeholder for an empty feed */}
       {/* TODO - build up a list of results */}
       {/* TODO [STRETCH] - update this list to be a list/grid of STRETCH_Cards */}
-      {/* <BookCard />
-      <CharacterCard />
-      <HouseCard /> */}
 
-      {
-           responseData && responseData.map((responseData, index) => {
-
-                    return <div key={responseData.name + '-' + index}>
-                        {sidebarCategories === 'Characters' && <CharacterCard character={responseData}/>}
-                        {sidebarCategories === 'Houses' && <HouseCard house={responseData} />}
-                        {sidebarCategories === 'Books' && <BookCard book={responseData} />}
-
-                    </div>
-                })
-
-        }
-
-
+      {responseData.length === 0 && (
+        <div className={"empty-feed-prompt"}>
+          <Text textColor="disabled">Nothing to show...</Text>
+        </div>
+      )}
+      {responseData &&
+        responseData.map((responseData, index) => {
+          return (
+            <div key={responseData.name + "-" + index}>
+              {sidebarCategories === "Characters" && (
+                <CharacterCard character={responseData} />
+              )}
+              {sidebarCategories === "Houses" && (
+                <HouseCard house={responseData} />
+              )}
+              {sidebarCategories === "Books" && (
+                <BookCard book={responseData} />
+              )}
+            </div>
+          );
+        })}
     </div>
-
   );
 };
--- src/components/STRETCH_Cards/CharacterCard.jsx
+++ src/components/STRETCH_Cards/CharacterCard.jsx
@@ -1,77 +1,70 @@
-import React, {useState} from 'react';
-import './Card.css';
-import {
-    Badge,
-    Heading
-  } from "@brandwatch/axiom-components";
+import React, { useState } from "react";
+import "./Card.css";
+import { Badge, Heading } from "@brandwatch/axiom-components";
 
 // TODO - create a component which displays information about Characters
 
 // TODO - make sure CharacterCard is expecting the right props!
-export const CharacterCard = ({character}) => {
+export const CharacterCard = ({ character }) => {
+  return (
+    <div className={"feed"} style={{ height: "100%", width: "100%" }}>
+      <div className="card">
+        <div className={"characters"} style={{ height: "100%", width: "100%" }}>
+          <Heading textSize={"headtitle"}>
+            <h2>{character.name || character.aliases[0]}</h2>
+          </Heading>
 
-      return <div className={'feed'} style={{height: "100%", width: "100%"}}>
-  
+          <div className="details" key="index">
+            {character.gender === "Female" && (
+              <Heading textSize={"large"}>
+                <strong>👩🏽‍⚖️</strong>: {character.gender}
+              </Heading>
+            )}
 
-              <div className="card">
+            {character.gender === "Male" && (
+              <Heading textSize={"large"}>
+                <strong>🧑🏽‍⚖️</strong>: {character.gender}
+              </Heading>
+            )}
 
+            {character.playedBy[0] !== "" && (
+              <Heading textSize={"large"}>
+                <strong>Played By</strong>: {character.playedBy}
+              </Heading>
+            )}
 
-        <div className={"characters"} style={{height: "100%", width: "100%"}}>
-           
-                <Heading textSize={'headtitle'}>
-                <h2>{character.name || character.aliases[0]}</h2>
-                </Heading>
+            {character.born !== "" && (
+              <Heading textSize={"large"}>
+                <strong>Birth</strong>: {character.born}
+              </Heading>
+            )}
 
+            {character.culture !== "" && (
+              <Heading textSize={"medium"}>
+                <strong>Culture</strong>: {character.culture}
+              </Heading>
+            )}
 
-                <div className="details" key="index">
-
-                  {
-                    character.gender === 'Female' && <Heading textSize={'large'}>
-                    <strong>👩🏽‍⚖️</strong>: {character.gender}
-                    </Heading>
-                  }
-
-                  {
-                    character.gender === 'Male' && <Heading textSize={'large'}>
-                    <strong>🧑🏽‍⚖️</strong>: {character.gender}
-                    </Heading>
-                  }
-
-                  {
-                    character.playedBy[0] !== '' && <Heading textSize={'large'}>
-                    <strong>Played By</strong>: {character.playedBy}
-                    </Heading>
-                  }
-
-                  {
-                    character.born !== '' && <Heading textSize={'large'}>
-                    <strong>Birth</strong>: {character.born}
-                    </Heading>
-                  }
-
-                  {
-                    character.culture !== '' && <Heading textSize={'medium'}>
-                    <strong>Culture</strong>: {character.culture}
-                    </Heading>
-                  }
-
-                  {
-                      character.titles[0] !== '' && <Heading textSize={'small'}>
-                    <strong>Titles</strong>: <em>{character.titles.join(', ')}</em>
-                    </Heading>
-                  }
-                  {
-                      character.aliases[0] !== '' && <Heading textSize={'small'}>
-                    <strong>Other Names</strong>: <em>{character.aliases.join(', ')}</em>
-                    </Heading>
-                  }
-                <Badge size={'small'} color={character.died ? 'tiny-clanger': 'terra-form'}>
-                    {character.died || 'Alive'}
-                </Badge>
-                </div>
-              </div>
-
-              </div>
-      
+            {character.titles[0] !== "" && (
+              <Heading textSize={"small"}>
+                <strong>Titles</strong>: <em>{character.titles.join(", ")}</em>
+              </Heading>
+            )}
+            {character.aliases[0] !== "" && (
+              <Heading textSize={"small"}>
+                <strong>Other Names</strong>:{" "}
+                <em>{character.aliases.join(", ")}</em>
+              </Heading>
+            )}
+            <Badge
+              size={"small"}
+              color={character.died ? "tiny-clanger" : "terra-form"}
+            >
+              {character.died || "Alive"}
+            </Badge>
+          </div>
+        </div>
       </div>
+    </div>
+  );
 };
